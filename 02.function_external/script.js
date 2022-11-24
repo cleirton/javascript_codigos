5edc9ab17e184d8e48a868e18872b6e05dc70629
Cleirton Gomes de Brito <cleirton.gomes@gmail.com>
2022-11-23 22:52:40 -0300

    JavaScript
    visualizando dados, declarações, operadores, variáveis e loops

---
+++ 02.function_external/script.js
@@ -0,0 +1,4 @@
+function mudandoTextoExterno(){
+    document.getElementById('evento3').innerHTML = 'Texto modificado externamente.';
+    document.getElementById('evento3').style.backgroundColor = 'lightblue';
+}
